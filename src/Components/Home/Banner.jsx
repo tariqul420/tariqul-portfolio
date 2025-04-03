42864433a5e24992857604bd66c0f3482f0e33ad
tariqul420 <tariqul.developer@gmail.com>
2025-04-03 20:35:42 +0600

    Enhance Banner component with code display and update styling; add window-dot hover effect in CSS

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,120 +1,95 @@
-import { Facebook, Github, Linkedin, Mail } from "lucide-react";
-import BestQuality from "../../assets/Images/quality.svg";
-import Responsive from "../../assets/Images/responsive.svg";
-import Robot from "../../assets/Images/robo.png";
-import { RainbowButton } from "../ui/rainbow-button";
+import { Code, Facebook, Github, Linkedin, Mail } from 'lucide-react';
+import { useState } from 'react';
+import { RainbowButton } from '../ui/rainbow-button';
 
 const Banner = () => {
-    const resumeLink = "https://drive.google.com/uc?export=download&id=10KuivzPbIRA8GThsQXZc2BQvXG96xWvy";
+  const resumeLink = 'https://drive.google.com/uc?export=download&id=10KuivzPbIRA8GThsQXZc2BQvXG96xWvy';
 
-    const handelResumeDownload = () => {
-        const link = document.createElement("a");
-        link.href = resumeLink;
-        link.setAttribute("download", "Resume of Md Tariqul Islam.pdf");
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
+  const handelResumeDownload = () => {
+    const link = document.createElement('a');
+    link.href = resumeLink;
+    link.setAttribute('download', 'Resume of Md Tariqul Islam.pdf');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
-    return (
-        <section
-            className=" -mt-[5.1rem] pt-28 pb-8 "
-        >
-            <div className="w-11/12 mx-auto flex items-center flex-col-reverse lg:flex-row gap-20">
-                {/* Text Section */}
-                <div className="flex-1 items-center justify-center">
-                    <p className="text-dark-deep text-[2.5rem] md:text-[3.5rem] lg:text-[2.5rem] font-semibold dark:text-white-lite text-center lg:text-left">
-                        Hi, I&apos;m
-                    </p>
-                    <h1 className="text-dark-deep text-[3.5rem] md:text-[4.5rem] lg:text-[3.5rem] font-bold dark:text-white-lite text-center lg:text-left max-sm:text-5xl">
-                        Tariqul Islam
-                    </h1>
-                    <p className="text-xl text-dark-lite font-medium dark:text-white-deep lg:text-left text-center">
-                        Front-end Web Developer
-                    </p>
+  const [code] = useState(`
+  const profile = {
+    name: 'Tariqul Islam',
+    title: 'Full-Stack Developer | MERN Stack Developer',
+    skills: ['React', 'NextJS', 'Express', Tailwind CSS,
+    'MongoDB', 'TypeScript', Javascript, Git, Firebase],
+    hardWorker: true,
+    quickLearner: true,
+    problemSolver: true,
+    yearsOfExperience: 1,
+    hireable: function () {
+      return(
+        this.hardWorker && 
+        this.problemSolver && 
+        this.skills.length >= 5 && 
+        this.yearsOfExperience >= 1
+      );
+    },
+  };
+  `);
 
-                    {/* Social Links */}
-                    <div className="flex gap-3 items-center justify-center lg:justify-start mt-3">
-                        <a
-                            href="https://www.linkedin.com/in/tariqul-420-t/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            aria-label="LinkedIn"
-                        >
-                            <Linkedin size={30} className="hero-icon lang" />
-                        </a>
-                        <a
-                            href="https://github.com/tariqul420"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            aria-label="GitHub"
-                        >
-                            <Github size={30} className="hero-icon lang" />
-                        </a>
-                        <a
-                            href="https://www.facebook.com/tariqul.islam.fb"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            aria-label="Facebook"
-                        >
-                            <Facebook size={30} className="hero-icon lang" />
-                        </a>
-                        <a className="lg:hidden"
-                            href="mailto:tariqul.developer@gmail.com"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            aria-label="Email"
-                        >
-                            <Mail size={30} className="hero-icon lang" />
-                        </a>
-                        <a
-                            className="hidden lg:block"
-                            href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=tariqul.developer@gmail.com"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            aria-label="Email"
-                        >
-                            <Mail size={30} className="hero-icon lang" />
-                        </a>
-                    </div>
+  return (
+    <section className="-mt-[5.1rem] pt-28 pb-8">
+      <div className="w-11/12 mx-auto lg:flex-row grid grid-cols-1 md:grid-cols-2 items-center">
+        {/* Text Section */}
+        <div className="flex-1 items-center justify-center">
+          <p className="text-dark-deep text-[2.5rem] md:text-[3.5rem] lg:text-[2.5rem] font-semibold dark:text-white-lite text-center lg:text-left">Hi, I&apos;m</p>
+          <h1 className="text-dark-deep text-[3.5rem] md:text-[4.5rem] lg:text-[3.5rem] font-bold dark:text-white-lite text-center lg:text-left max-sm:text-5xl">Tariqul Islam</h1>
+          <p className="text-xl text-dark-lite font-medium dark:text-white-deep lg:text-left text-center">Full Stack Developer | Problem Solver</p>
 
-                    <div className="mt-8 flex items-center justify-center lg:justify-start">
-                        <RainbowButton
-                            className={`!bg-gradient-to-r from-primary2 to-primary1 rounded-xl text-white font-semibold custom-scale-effect`}
-                            onClick={handelResumeDownload}
-                        >
-                            Download Resume
-                        </RainbowButton>
-                    </div>
-                </div>
+          {/* Social Links */}
+          <div className="flex gap-3 items-center justify-center lg:justify-start mt-3">
+            <a href="https://www.linkedin.com/in/tariqul-420-t/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+              <Linkedin size={30} className="hero-icon lang" />
+            </a>
+            <a href="https://github.com/tariqul420" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+              <Github size={30} className="hero-icon lang" />
+            </a>
+            <a href="https://www.facebook.com/tariqul.islam.fb" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+              <Facebook size={30} className="hero-icon lang" />
+            </a>
+            <a className="lg:hidden" href="mailto:tariqul.developer@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email">
+              <Mail size={30} className="hero-icon lang" />
+            </a>
+            <a className="hidden lg:block" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=tariqul.developer@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email">
+              <Mail size={30} className="hero-icon lang" />
+            </a>
+          </div>
 
-                {/* Image Section */}
-                <div className="flex-1 relative">
-                    <img
-                        className="w-full md:h-[50rem] h-[35rem] lg:h-[35rem] object-contain"
-                        src={Robot}
-                        alt="Hero Illustration"
-                    />
+          <div className="mt-8 flex items-center justify-center lg:justify-start">
+            <RainbowButton className={`!bg-gradient-to-r from-primary2 to-primary1 rounded-xl text-white font-semibold custom-scale-effect`} onClick={handelResumeDownload}>
+              Download Resume
+            </RainbowButton>
+          </div>
+        </div>
 
-                    {/* Responsive Badge */}
-                    <div className="flex items-center gap-4 px-8 py-2 rounded-xl shadow-3xl backdrop-blur-xl absolute top-[80%] left-0 dark:bg-[#25a25785] border-t-[3px] border-r-2 border-solid border-primary2/40 responsive">
-                        <img src={Responsive} alt="Responsive Icon" />
-                        <p className="dark:text-white-deep font-semibold text-xl">
-                            Responsive
-                        </p>
-                    </div>
-
-                    {/* Quality Badge */}
-                    <div className="flex items-center gap-4 px-8 py-3 rounded-xl shadow-3xl backdrop-blur-xl absolute top-[55%] right-0 dark:bg-[#25a25785] border-t-[3px] border-l-2 border-solid border-primary2/40 bestQuality">
-                        <img src={BestQuality} alt="Quality Icon" />
-                        <p className="dark:text-white-deep font-semibold text-xl">
-                            Best Quality
-                        </p>
-                    </div>
-                </div>
+        {/* Right column - Code window */}
+        <div className="flex-1 p-4">
+          <div className="bg-white dark:bg-dark-lite rounded-xl overflow-hidden border dark:border-[#172635]">
+            <div className="flex items-center gap-2 p-3 bg-gray-100 dark:bg-gray-800">
+              <div className="w-3 h-3 rounded-full bg-red-500 hover:bg-red-600 transition-colors"></div>
+              <div className="w-3 h-3 rounded-full bg-yellow-500 hover:bg-yellow-600 transition-colors"></div>
+              <div className="w-3 h-3 rounded-full bg-green-500 hover:bg-green-600 transition-colors"></div>
+              <span className="ml-2 text-sm font-medium text-gray-600 dark:text-gray-300 flex items-center gap-2">
+                <Code size={16} />
+                developer.js
+              </span>
             </div>
-        </section>
-    );
+            <pre className="overflow-x-auto">
+              <code>{code}</code>
+            </pre>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
